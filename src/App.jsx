bb69601bbdf9fa3d608c97a8c1f333705540149d
Gaganharikiranmarella <ghk7125@gmail.com>
2025-05-26 13:18:02 +0530

    fixed errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-// src/App.jsx
 import { Routes, Route } from 'react-router-dom';
 import Home from './Home';
 
-// Import your algorithm components
+// ✅ Import only the correct algorithm components
 import Dijkstra from './algorithms/Dijkstra';
 import JobSequencing from './algorithms/JobSequencing';
-import NQueens from './algorithms/NQueens';
 import Knapsack from './algorithms/Knapsack';
-import Kruskal from './algorithms/Kruskal';
-import Prims from './algorithms/Prims';
+import NQueens from './algorithms/NQueens';
 import BFS from './algorithms/BFS';
 import DFS from './algorithms/DFS';
 
@@ -18,10 +15,8 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/dijkstra" element={<Dijkstra />} />
       <Route path="/jobsequencing" element={<JobSequencing />} />
-      <Route path="/nqueens" element={<NQueens />} />
       <Route path="/knapsack" element={<Knapsack />} />
-      <Route path="/kruskal" element={<Kruskal />} />
-      <Route path="/prims" element={<Prims />} />
+      <Route path="/nqueens" element={<NQueens />} />
       <Route path="/bfs" element={<BFS />} />
       <Route path="/dfs" element={<DFS />} />
     </Routes>
